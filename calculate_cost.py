5b92b12ebaa2cdec8f510f023611be7f03e5987f
edemirkirkan <ege.demirkirkan@ug.bilkent.edu.tr>
2022-04-09 04:39:51 +0800

    Add encapsulation function

--- calculate_cost.py
+++ calculate_cost.py
@@ -2,16 +2,24 @@ from urllib.request import urlopen
 import json
 
 def main():
-    # Additional samples can be added to following set for testing purposes
+    # additional samples can be added to following set for testing purposes
     sample_urls = ['https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_1.json', 
     'https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_2.json', 
     'https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_3.json']
+    
+    run(sample_urls)
 
+def run(sample_urls):   
     for sample_number, url in enumerate(sample_urls, 1):
         json = retrieve_json(url)
         name = json['name']
         cost = calculate_cost(json)
         print("Case {}: Cost of item '{}' is {} units.".format(sample_number, name, cost))
+
+def retrieve_json(url):
+    json_url = urlopen(url)
+    data = json.loads(json_url.read())
+    return data
 
 def calculate_cost(json):
     item_cost = 0
@@ -22,12 +30,6 @@ def calculate_cost(json):
             item_cost += calculate_cost(item)
     item_cost *= json['count']    
     return item_cost
-        
-
-def retrieve_json(url):
-    json_url = urlopen(url)
-    data = json.loads(json_url.read())
-    return data
 
 def is_item_part(item):
     try:
